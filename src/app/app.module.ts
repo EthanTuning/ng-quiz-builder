a2b92999124923a5bbd44aa21645523b045b6400
Ethan Tuning <etuning5@hotmail.com>
2019-06-05 14:54:43 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/editor-components/header/header.component';
+import { SimpleimageComponent } from './components/editor-components/simpleimage/simpleimage.component';
+import { QuoteComponent } from './components/editor-components/quote/quote.component';
+import { CodeComponent } from './components/editor-components/code/code.component';
+import { TableComponent } from './components/editor-components/table/table.component';
+import { DelimeterComponent } from './components/editor-components/delimeter/delimeter.component';
+import { ChecklistComponent } from './components/editor-components/checklist/checklist.component';
+import { ListComponent } from './components/editor-components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SimpleimageComponent,
+    QuoteComponent,
+    CodeComponent,
+    TableComponent,
+    DelimeterComponent,
+    ChecklistComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/editor-components/delimeter/delimeter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delimeter',
+  templateUrl: './delimeter.component.html',
+  styleUrls: ['./delimeter.component.css']
+})
+export class DelimeterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
