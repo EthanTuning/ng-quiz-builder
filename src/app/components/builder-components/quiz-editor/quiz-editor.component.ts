8d4d5ffe2f184913f764e163fb047ccc8df419ac
Ethan Tuning <etuning5@hotmail.com>
2019-06-10 12:51:15 -0700

    added initial quiz configuration with some instructions

--- src/app/components/builder-components/quiz-editor/quiz-editor.component.ts
+++ src/app/components/builder-components/quiz-editor/quiz-editor.component.ts
@@ -19,7 +19,9 @@ import EditorJS from '@editorjs/editorjs';
 
 export class QuizEditorComponent implements OnInit {
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
 
     const editor = new EditorJS({
       holderId: 'quiz-editor-container',
@@ -69,16 +71,55 @@ export class QuizEditorComponent implements OnInit {
           {
             type: 'header',
             data: {
-              text: 'Question #1',
+              text: 'Quiz Title',
               level: 1
             }
+          },
+          {
+            type: 'header',
+            data: {
+              text: 'Name: ',
+              level: 3
+            }
+          },
+          {
+            type: 'header',
+            data: {
+              text: 'Question #1',
+              level: 4
+            }
+          },
+          {
+            type: 'paragraph',
+            data: {
+              text: 'This is the first question of the quiz. ' +
+              'To add more content, use the block editor! ' +
+              'To add a block, hit the enter key after your ' +
+              'current block. To edit a block, use the settings to the ' +
+              'right of the editor window. As you edit, hit the ' +
+              '"Preview" button above to see what the quiz will actually ' +
+              'look like.'
+            }
+          },
+          {
+            type: 'list',
+            data: {
+              style: 'ordered',
+              items: [
+                'Multiple',
+                'Choice',
+                'Answer',
+                'Example'
+              ]
+            }
+          },
+          {
+            type: 'delimiter',
+            data: {}
           }
         ]
       }
     });
- }
-
-  ngOnInit() {
   }
 
 }
